8cdfa29b596519b94b54deab46d616c8c78513fc
gabirelcha18 <gabrielcha@qq.com>
2021-08-02 13:52:30 -0700

    project fuction file create

--- analysis/Gabrielcha/scripts/project_functions.py
+++ analysis/Gabrielcha/scripts/project_functions.py
@@ -1,33 +1,36 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "8f84be10-5c85-4155-8773-ced8179d6859",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.10"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pandas_profiling import ProfileReport
+
+# Method chaining begins
+
+# Method Chain 1 (Load data and clean data and process )
+
+
+def load_and_process(url_or_path_to_csv_file):
+    
+    data=pd.read_csv(url_or_path_to_csv_file)
+
+    df1 = (
+              data[(data.year >=2008) & (data.year <=2018)]
+             .drop(['Life Ladder', 'Log GDP per capita','Social support','Generosity','Perceptions of corruption','Positive affect', 'Negative affect'],axis=1) 
+             .rename(columns={'Healthy life expectancy at birth':'life_expectancy','Freedom to make life choices':'Freedom_life_choices_in_percentage'})
+             .assign(Freedom_life_choices_in_percentage=lambda x: x['Freedom_life_choices_in_percentage']*100)
+             .assign(life_expectancy=lambda x: x['life_expectancy'].fillna(df1.groupby('Country name')['life_expectancy'].transform('mean')))
+    )
+
+#method chain 2 (Load data and clean data and process )
+
+    df2 = (
+            df1
+            .assign(Freedom_life_choices_in_percentage=lambda x: x['Freedom_life_choices_in_percentage'].fillna(df1.groupby('Country name')['Freedom_life_choices_in_percentage'].transform('mean')))
+            .fillna(0,axis='columns')
+            .loc[(df1['life_expectancy'] != 0) ]
+            .groupby('Country name')
+            .filter(lambda x : len(x)>10)
+
+         )
+    
+    return df2
