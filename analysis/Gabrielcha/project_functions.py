ae777bd703f04f775fe1fea2bbe8f921b44a137c
gabirelcha18 <gabrielcha@qq.com>
2021-08-13 13:19:14 -0700

    gabirelcha finished milestone3

--- analysis/Gabrielcha/project_functions.py
+++ analysis/Gabrielcha/project_functions.py
@@ -6,19 +6,19 @@ import seaborn as sns
 def load_and_process(path):
     
     df0 = (
-           pd.read_csv(path)
-           .drop(['Life Ladder', 'Log GDP per capita','Social support','Generosity','Perceptions of corruption','Positive affect', 'Negative affect'],axis=1)
+           pd.read_csv(path)   # load the data
+           .drop(['Life Ladder', 'Log GDP per capita','Social support','Generosity','Perceptions of corruption','Positive affect', 'Negative affect'],axis=1)   # drop the unsed column
         
            )
     
     
     df1 = (
               df0
-              .loc[df0.year >= 2008]
-              .loc[df0.year <= 2018]
-             .rename(columns={'Healthy life expectancy at birth':'life_expectancy','Freedom to make life choices':'Freedom_life_choices_in_percentage'})
+              .loc[df0.year >= 2008]   # keeping the data after 2008 inclusive
+              .loc[df0.year <= 2018]    # keeping the data before 2018 inclusive
+             .rename(columns={'Healthy life expectancy at birth':'life_expectancy','Freedom to make life choices':'Freedom_life_choices_in_percentage'})    # rename some of the columns
              .assign(Freedom_life_choices_in_percentage=lambda x: x['Freedom_life_choices_in_percentage']*100)
-        
+             # calculate the column let it become percentage
             
            )
 
@@ -39,6 +39,6 @@ def load_and_process(path):
 
 
 # note
-# line #1,2 for some region,some year have data, therefore filling with the acverage number 
+# line #1,2 for some region,some year have data, therefore filling with the average number 
 # line #3 delete the country where do not any record within the given year period
 # only keeping the country have more than 10 records
